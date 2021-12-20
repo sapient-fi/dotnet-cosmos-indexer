649943999f6139ba57ab35f72eb4b9e9e408e154
laine-ust <94804332+laine-ust@users.noreply.github.com>
2021-12-20 21:29:13 +0100

    Add storage for MINE buyback transactions

---
+++ src/ServiceHost/DAL/TerraMoney/Migrations/Migration_20211220_202500_MineBuybacks.cs
@@ -0,0 +1,102 @@
+using System.Text.Json;
+using Pylonboard.Kernel;
+using Pylonboard.Kernel.IdGeneration;
+using Pylonboard.ServiceHost.DAL.Exchanges;
+using Pylonboard.ServiceHost.DAL.TerraMoney;
+using Pylonboard.ServiceHost.Oracles;
+using Pylonboard.ServiceHost.Oracles.ExchangeRates.Terra;
+using Pylonboard.ServiceHost.Oracles.TerraFcd.Messages;
+using Pylonboard.ServiceHost.TerraDataFetchers;
+using RapidCore.Migration;
+using RapidCore.PostgreSql.Migration;
+using ServiceStack;
+using ServiceStack.OrmLite;
+using ServiceStack.OrmLite.Dapper;
+
+namespace Invacoil.Data.Migrations
+{
+    public class Migration_20211220_202500_MineBuybacks : MigrationBase
+    {
+        protected override void ConfigureUpgrade(IMigrationBuilder builder)
+        {
+            var ctx = ContextAs<PostgreSqlMigrationContext>();
+            var connection = ctx.ConnectionProvider.Default();
+            builder.Step("add mine buybacks table", () =>
+            {
+                connection.Execute(@"
+create table terra_mine_buyback_entity
+(
+    id               bigint                   not null
+        primary key,
+    transaction_id   bigint                   not null,
+    transaction_hash text,
+    mine_amount      numeric(38, 6)           not null,
+    ust_amount       numeric(38, 6)           not null,
+    created_at       timestamp with time zone not null
+);
+");
+            });
+            builder.Step("fill table with buyback info", async () =>
+            {
+                var transactionEnumerator = ctx.Container.Resolve<TerraTransactionEnumerator>();
+                var idGenerator = ctx.Container.Resolve<IdGenerator>();
+                
+                await foreach (var (tx, stdTx) in transactionEnumerator.EnumerateTransactionsAsync(
+                           0,
+                           100,
+                           TerraStakingContracts.MINE_BUYBACK_CONTRACT,
+                           CancellationToken.None
+                       ))
+                {
+                    await connection.SaveAsync(obj: new TerraRawTransactionEntity
+                        {
+                            Id = tx.Id,
+                            CreatedAt = tx.CreatedAt,
+                            TxHash = tx.TransactionHash,
+                            RawTx = JsonSerializer.Serialize(tx),
+                        }
+                    );
+                    
+                    var events = tx.Logs
+                        .SelectMany(l => l.Events)
+                        .Where(evt =>
+                            evt.Attributes.Contains(new TxLogEventAttribute() { Key = "action", Value = "sweep" }) &&
+                            evt.Type.EqualsIgnoreCase("from_contract")
+                        ).ToList();
+
+                    if (!events.Any())
+                    {
+                        continue;
+                    }
+
+                    foreach (var evt in events)
+                    {
+                        var distributeMineAmountStr =
+                            evt.Attributes.First(attrib => 
+                                attrib.Key.EqualsIgnoreCase("distribute_amount")).Value;
+                        var mineAmount = distributeMineAmountStr.ToInt64() / 1_000_000M;
+                        var offerAmountUstStr =
+                            evt.Attributes.First(attribute =>
+                                attribute.Key.EqualsIgnoreCase("offer_amount")).Value;
+                        var offerAmountUst = offerAmountUstStr.ToInt64() / 1_000_000m;
+                        
+                        await connection.InsertAsync(new TerraMineBuybackEntity
+                        {
+                            Id = idGenerator.Snowflake(),
+                            CreatedAt = tx.CreatedAt,
+                            MineAmount = mineAmount,
+                            UstAmount = offerAmountUst,
+                            TransactionHash = tx.TransactionHash,
+                            TransactionId = tx.Id,
+                        });
+                    }
+                }
+            });
+        }
+
+        protected override void ConfigureDowngrade(IMigrationBuilder builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/ServiceHost/DAL/TerraMoney/TerraMineBuybackEntity.cs
@@ -0,0 +1,16 @@
+namespace Pylonboard.ServiceHost.DAL.TerraMoney;
+
+public record TerraMineBuybackEntity
+{
+    public long Id { get; set; }
+
+    public long TransactionId { get; set; }
+
+    public string TransactionHash { get; set; }
+
+    public decimal MineAmount { get; set; }
+    
+    public decimal UstAmount { get; set; }
+    
+    public DateTimeOffset CreatedAt { get; set; }
+}
