5be44c26b00ab0a428d50d506961fb4265afe6e2
laine-ust <94804332+laine-ust@users.noreply.github.com>
2022-02-04 14:55:38 +0100

    Add additional helper methods to the tx log extensions
    To get the LAST or default entry of a given match

--- src/ServiceHost/Extensions/TxLogExtensions.cs
+++ src/ServiceHost/Extensions/TxLogExtensions.cs
@@ -31,6 +31,33 @@ public static class TxLogExtensions
                 predicate
             );
     }
+    
+    public static TxLogEventAttribute? QueryTxLogsForAttributeLastOrDefault(
+        this List<TxLog> logs,
+        string txLogEventPropToFind,
+        string attributeKeyToFind
+    )
+    {
+        return logs.QueryTxLogsForAttributeLastOrDefault(
+            txLogEventPropToFind,
+            attribute => attribute.Key == attributeKeyToFind);
+    }
+    
+    public static TxLogEventAttribute? QueryTxLogsForAttributeLastOrDefault(
+        this List<TxLog> logs,
+        string txLogEventPropToFind,
+        Func<TxLogEventAttribute, bool> predicate
+    )
+    {
+        return logs
+            .SelectMany(l =>
+                l.Events)
+            .Where(e => e.Type.EqualsIgnoreCase(txLogEventPropToFind))
+            .SelectMany(evt => evt.Attributes)
+            .LastOrDefault(
+                predicate
+            );
+    }
     public static IEnumerable<TxLogEventAttribute> QueryTxLogsForAttributes(
         this List<TxLog> logs,
         string txLogEventPropToFind,
