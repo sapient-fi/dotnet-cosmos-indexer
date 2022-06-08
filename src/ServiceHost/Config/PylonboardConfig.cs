9e28f422354a69b8795be6f10c05bf878c2ed1ed
0xlaine <94804332+0xlaine@users.noreply.github.com>
2022-06-08 18:58:31 +0200

    Rename all config variable prefixes

--- src/ServiceHost/Config/PylonboardConfig.cs
+++ src/ServiceHost/Config/PylonboardConfig.cs
@@ -12,7 +12,7 @@ public class PylonboardConfig : IEnabledServiceRolesConfig, IDbConfig, IGatewayP
         _config = config;
     }
 
-    public List<string> EnabledRoles => _config.GetFromCommaSeparatedList<string>("PYLONBOARD_SERVICE_ROLES_ENABLED", new List<string>
+    public List<string> EnabledRoles => _config.GetFromCommaSeparatedList<string>("SAPIENT_SERVICE_ROLES_ENABLED", new List<string>
     {
         ServiceRoles.API,
         ServiceRoles.BACKGROUND_WORKER,
@@ -24,29 +24,29 @@ public class PylonboardConfig : IEnabledServiceRolesConfig, IDbConfig, IGatewayP
     }
 
     string IDbConfig.ConnectionString => _config.Get(
-        "PYLONBOARD_DB_CONNECTION_STRING",
-        "User ID=pylonboard_user;Password=pylonboard_user_pass;Host=localhost;Port=35432;Database=pylonboard;Pooling=true;Minimum Pool Size=10;Maximum Pool Size=100;"
+        "SAPIENT_DB_CONNECTION_STRING",
+        "User ID=sapient_user;Password=sapient_user_pass;Host=localhost;Port=35432;Database=pylonboard;Pooling=true;Minimum Pool Size=10;Maximum Pool Size=100;"
     );
 
     bool IDbConfig.DisableMigrationsDuringBoot => _config.Get(
-        "PYLONBOARD_DB_DISABLE_MIGRATIONS_ON_BOOT",
+        "SAPIENT_DB_DISABLE_MIGRATIONS_ON_BOOT",
         false
     );
 
     int IGatewayPoolsConfig.NumberOfElementsInDepositsPrWallet => _config.Get(
-        "PYLONBOARD_NR_ELEMENTS_DEPOSITS_PR_WALLET",
+        "SAPIENT_NR_ELEMENTS_DEPOSITS_PR_WALLET",
         11
     );
 
-    List<string> ICorsConfig.AllowedOrigins => _config.GetFromCommaSeparatedList<string>("PYLONBOARD_API_ALLOWED_CORS_ORIGINS", new List<string>
+    List<string> ICorsConfig.AllowedOrigins => _config.GetFromCommaSeparatedList<string>("SAPIENT_API_ALLOWED_CORS_ORIGINS", new List<string>
     {
         "http://localhost:3000",
     });
 
-    bool IFeatureConfig.TriggerGatewayPoolFullResync => _config.Get("PYLONBOARD_TRIGGER_GATEWAY_POOL_FULL_RESYNC", false);
+    bool IFeatureConfig.TriggerGatewayPoolFullResync => _config.Get("SAPIENT_TRIGGER_GATEWAY_POOL_FULL_RESYNC", false);
     
-    bool IFeatureConfig.TriggerMineStakingFullResync => _config.Get("PYLONBOARD_TRIGGER_MINE_STAKING_FULL_RESYNC", false);
-    bool IFeatureConfig.TriggerMineBuyBackFullResync  => _config.Get("PYLONBOARD_TRIGGER_MINE_BUYBACK_FULL_RESYNC", false);
+    bool IFeatureConfig.TriggerMineStakingFullResync => _config.Get("SAPIENT_TRIGGER_MINE_STAKING_FULL_RESYNC", false);
+    bool IFeatureConfig.TriggerMineBuyBackFullResync  => _config.Get("SAPIENT_TRIGGER_MINE_BUYBACK_FULL_RESYNC", false);
     
-    string IMessageTransportConfig.TransportUri => _config.Get("PYLONBOARD_MESSAGE_TRANSPORT_URI", "amqp://guest:guest@localhost:5672");
+    string IMessageTransportConfig.TransportUri => _config.Get("SAPIENT_MESSAGE_TRANSPORT_URI", "amqp://guest:guest@localhost:5672");
 }
